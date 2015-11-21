92c707ced43f64fc6caa40898d27013c860094b5
NickvanDyke <nickjvandyke15@gmail.com>
2015-11-20 23:24:24 -0600

    Few changes
    Previously, if an ad's html didn't contain a location, the new ad would
    just use the previous ad's location, resulting in the user being
    notified of the same ad multiple times if it didn't have a location.
    Also made some of the substrings more specific to ensure they would
    parse the html correctly.

--- CraigslistNotifier.java
+++ CraigslistNotifier.java
@@ -415,7 +415,7 @@ public final class CraigslistNotifier {
 			return result;
 		}
 		Ad temp;
-		String title, date, location = "n/a", link, city;
+		String title, date, location, link, city;
 		String[] splitHtml;
 		int price;
 		city = str.substring(str.indexOf("<option value=\"") + 15, str.indexOf("</option>"));
@@ -424,13 +424,14 @@ public final class CraigslistNotifier {
 			splitHtml =  str.substring(str.indexOf("<p"), str.indexOf("<h4")).split("</p>");
 		else splitHtml = str.substring(str.indexOf("<p"), str.indexOf("<div id=\"mapcontainer")).split("</p>");
 		for (String adHtml : splitHtml) {
-			title = adHtml.substring(adHtml.indexOf("hdrlnk") + 8, adHtml.indexOf("</a> </span>")).replace("&amp;", "&");
-			date = adHtml.substring(adHtml.indexOf("title") + 7, adHtml.indexOf("title") + 29);
+			title = adHtml.substring(adHtml.indexOf("hdrlnk\">") + 8, adHtml.indexOf("</a> </span>")).replace("&amp;", "&");
+			date = adHtml.substring(adHtml.indexOf("title=\"") + 7, adHtml.indexOf("title=\"") + 29);
 			if (adHtml.contains("<small>"))
 				location = adHtml.substring(adHtml.indexOf("<small>") + 9, adHtml.indexOf("</small>") - 1);
-			link = "https://" + city + ".craigslist.org" + adHtml.substring(adHtml.indexOf("href") + 6, adHtml.indexOf("html") + 4);
+			else location = "n/a";
+			link = "https://" + city + ".craigslist.org" + adHtml.substring(adHtml.indexOf("href=\"") + 6, adHtml.indexOf("html\"") + 4);
 			if (adHtml.contains("price"))
-				price = Integer.parseInt(adHtml.substring(adHtml.indexOf("price") + 8, adHtml.indexOf("</span")));
+				price = Integer.parseInt(adHtml.substring(adHtml.indexOf("price\">") + 8, adHtml.indexOf("</span")));
 			else price = 0;
 			temp = new Ad(title, price, date, location, link);
 			result.add(temp);
